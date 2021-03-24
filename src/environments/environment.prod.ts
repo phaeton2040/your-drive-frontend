246882a45700c0aa07a165a13f6e8983e4737af1
phaeton2040 <phaeton2040@gmail.com>
2021-03-24 12:41:31 +0300

    Unit tests workflow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
+export const environment: any = {
   production: true,
-  apiBaseUrl: 'https://4s5s1spega.execute-api.eu-central-1.amazonaws.com'
+  apiBaseUrl: 'https://p7ko0vtgy8.execute-api.eu-central-1.amazonaws.com/dev',
+  appName: 'yourdisk',
+  storeName: 'yourdisk-store-dev'
 };
