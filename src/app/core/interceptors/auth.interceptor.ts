05962278034c02777c0c41e4180c9d9f1637f82a
phaeton2040 <phaeton2040@gmail.com>
2021-03-24 23:43:23 +0300

    Origin isn't set

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -23,8 +23,7 @@ export class AuthInterceptor implements HttpInterceptor {
           if (token) {
             request = request.clone({
               setHeaders: {
-                Authorization: `Bearer ${token}`,
-                Origin: '*'
+                Authorization: `Bearer ${token}`
               }
             });
           }
